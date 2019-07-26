4fc633b31cb78b5cd6f1616bb014e92ac7573e2d
paulQR <paulqr10@gmail.com>
2019-07-26 09:42:50 +0200

    2 files modified

--- app/pages/tab1/tab1.ts
+++ app/pages/tab1/tab1.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { DeseosService } from '../../services/deseos.service';
+//import { AlertController } from '@ionic/angular';
 import { AgregarPage } from '../agregar/agregar';
+
 
 @Component({
   selector: 'page-tab1',
